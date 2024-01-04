c3e04685eda955bc4719a7ebb614244c47c5d102
javiervargas02 <javier.v.021204@gmail.com>
2024-01-03 23:28:03 -0600

    Create component to display reviews
    This component was a challenge to complete. The component starts with the specified amount of empty stars. If the user hovers over a star it shows how many stars are going to be filled when the user clicks. If the user clicks, the rating is saved to the state and the amount of stars selected become full, if not, the stars stay as they were before the user hovered over.

---
+++ src/components/StarRating.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import Star from "./Star";
+
+const containerStyle = {
+  display: "flex",
+  alignItems: "center",
+  gap: 16,
+};
+
+const starContainerStyle = {
+  display: "flex",
+};
+
+export default function StarRating({
+  maxRating,
+  color = "#fcc419",
+  size = 25,
+}) {
+  
+  const textStyle = {
+    lineHeight: 0,
+    margin: 0,
+    color,
+    fontSize: size,
+  };
+
+  const [rating, setRating] = useState(0);
+  const [tempRating, setTempRating] = useState(0);
+  return (
+    <div style={containerStyle}>
+      <div style={starContainerStyle}>
+        {Array.from({ length: maxRating }, (_, i) => (
+          <Star
+            key={crypto.randomUUID()}
+            full={tempRating ? i + 1 <= tempRating : i + 1 <= rating}
+            onRate={() => {
+              setRating(i + 1);
+            }}
+            onHoverIn={() => setTempRating(i + 1)}
+            onHoverOut={() => setTempRating(0)}
+            size={size}
+            color={color}
+          />
+        ))}
+      </div>
+      <p style={textStyle}>{tempRating || rating || ""}</p>
+    </div>
+  );
+}
