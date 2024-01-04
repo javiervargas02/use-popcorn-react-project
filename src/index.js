7a3b00d3488ff89b2a024e476e4477d029866fdb
javiervargas02 <javier.v.021204@gmail.com>
2024-01-03 23:15:07 -0600

    Remove innecessary stuff and tested the functionality of different components

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// import StarRating from "./components/StarRating";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    {/* <StarRating maxRating={10} /> */}
     <App />
   </React.StrictMode>
 );
