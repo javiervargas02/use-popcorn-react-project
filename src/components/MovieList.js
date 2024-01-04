23d9f38ea0c23e32cdabc5c3f6ea2091e3496114
javiervargas02 <javier.v.021204@gmail.com>
2024-01-03 23:19:12 -0600

    Create component
    Made the component easy to read and understand

---
+++ src/components/MovieList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Movie from "./Movie";
+
+export default function MovieList({ movies }) {
+  return (
+    <ul className="list">
+      {movies?.map((movie) => (
+        <Movie movie={movie} key={movie.imdbID} />
+      ))}
+    </ul>
+  );
+}
