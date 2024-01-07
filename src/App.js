a35c64e79db38c05950308b09cf52a35d8938942
javiervargas02 <javier.v.021204@gmail.com>
2024-01-07 00:07:06 -0600

    Add prop to handle the add watched movie function

--- src/App.js
+++ src/App.js
@@ -105,6 +105,7 @@ export default function App() {
             <MovieDetail
               selectedId={selectedMovieId}
               closeMovie={() => setSelectedMovieId(null)}
+              addWatchedMovie={setWatched}
             />
           ) : (
             <>
