23e45f9301bfa05c04d4923296ec8779966e15ba
javiervargas02 <javier.v.021204@gmail.com>
2024-01-06 23:29:48 -0600

    Set selected movie id when the movie is clicked

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,8 +1,8 @@
 import React from "react";
 
-export default function Movie({ movie }) {
+export default function Movie({ movie, selectMovie }) {
   return (
-    <li>
+    <li onClick={() => selectMovie(movie.imdbID)}>
       <img src={movie.Poster} alt={`${movie.Title} poster`} />
       <h3>{movie.Title}</h3>
       <div>
