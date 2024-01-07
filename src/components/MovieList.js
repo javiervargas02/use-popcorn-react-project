0d548a2b6b1635ea8b80187a66b382a97159fb74
javiervargas02 <javier.v.021204@gmail.com>
2024-01-06 23:28:57 -0600

    Pass in props to use on nested component

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,11 +1,11 @@
 import React from "react";
 import Movie from "./Movie";
 
-export default function MovieList({ movies }) {
+export default function MovieList({ movies, selectMovie }) {
   return (
-    <ul className="list">
+    <ul className="list list-movies">
       {movies?.map((movie) => (
-        <Movie movie={movie} key={movie.imdbID} />
+        <Movie movie={movie} key={movie.imdbID} selectMovie={selectMovie} />
       ))}
     </ul>
   );
