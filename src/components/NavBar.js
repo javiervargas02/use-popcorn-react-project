1ca27c45acc49890f4ff76c623eb199822953c1f
javiervargas02 <javier.v.021204@gmail.com>
2024-01-06 22:56:11 -0600

    Remove Search component to lift it up and use component composition

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,10 @@
 import React from "react";
 import Logo from "./Logo";
-import Search from "./Search";
 
 export default function NavBar({ children }) {
   return (
     <nav className="nav-bar">
       <Logo />
-      <Search />
       {children}
     </nav>
   );
