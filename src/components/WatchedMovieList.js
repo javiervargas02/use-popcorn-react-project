e41484d43b77b0edaab414ba0562c72582d0bab2
javiervargas02 <javier.v.021204@gmail.com>
2024-01-21 17:19:57 -0600

    Pass down props to implement removing a movie

--- src/components/WatchedMovieList.js
+++ src/components/WatchedMovieList.js
@@ -1,11 +1,15 @@
 import React from "react";
 import WatchedMovie from "./WatchedMovie";
 
-export default function WatchedMovieList({ watched }) {
+export default function WatchedMovieList({ watched, handleRemove }) {
   return (
     <ul className="list">
       {watched.map((movie) => (
-        <WatchedMovie movie={movie} key={movie.imdbID} />
+        <WatchedMovie
+          movie={movie}
+          key={movie.imdbID}
+          handleRemove={handleRemove}
+        />
       ))}
     </ul>
   );
